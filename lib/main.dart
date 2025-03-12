a20f82d0fc0dd46cdf9af55bc53d0b0a05a1c1f0
John Rollins <john.rollinsjr07@gmail.com>
2025-03-12 19:29:19 -0400

    Made Floating Action Buttons

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class PetCareApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const HomePage(title: 'Home'),
+      home: const HomePage(title: 'Pet Care'),
     );
   }
 }
@@ -32,22 +32,27 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
+      backgroundColor: Color.fromARGB(255, 241, 255, 232),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            
-          ],
+          children: <Widget>[],
         ),
       ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: onpressed,
-      //   child: const Icon(Icons.add),
-      // ),
+      floatingActionButton: Column(
+        verticalDirection: VerticalDirection.up,
+        children: <Widget>[
+          FloatingActionButton(onPressed: () {}),
+          const SizedBox(height: 16),
+          FloatingActionButton(onPressed: () {}),
+          const SizedBox(height: 16),
+          FloatingActionButton(
+            onPressed: () {},
+            child: const Icon(Icons.question_mark, color: Colors.white),
+            shape: CircleBorder(),
+          ),
+        ],
+      ),
     );
   }
 }
